f5a2dc31a99a9eaca3ab95cb25abb91472cdc881
n-kikuchi-ij <55069063+n-kikuchi-ij@users.noreply.github.com>
2020-05-08 22:35:04 +0900

    add Dockerfile and oseti file

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:latest
+
+# generate workspace
+RUN mkdir /workdir
+
+#copy app file to container
+COPY app/ /workdir
+
+RUN apt-get update
+RUN apt-get install python3 python3-pip -y
+RUN apt-get install -y locales
+RUN locale-gen ja_JP.UTF-8
+RUN echo "export LANG=ja_JP.UTF-8" >> ~/.bashrc
+
+RUN pip3 install mecab-python3
+RUN pip3 install oseti
---
+++ app/today.py
@@ -0,0 +1,19 @@
+import oseti
+import sys, codecs, io
+
+print("今日の気分を入力してください")
+kibun = input()
+strkibun = str(kibun)
+
+a = oseti.Analyzer()
+analized_kibun = a.analyze(strkibun)
+
+score_for_today = analized_kibun[0]
+if score_for_today >= 1.0:
+    print("楽しそうで良かったです！")
+
+elif score_for_today < 0.0:
+    print("猫画像をどうぞ！")
+
+else:
+    print("良い日が続きますように。Have a nice day!")
