882ff6f9e1b0ecbb68cd0203ad33fd6f5bf35371
n-kikuchi-ij <55069063+n-kikuchi-ij@users.noreply.github.com>
2020-05-11 23:15:21 +0900

    generate templates

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hello world!!"
+def kibun():
+    return render_template('kibun.html', title='今日の気分')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
